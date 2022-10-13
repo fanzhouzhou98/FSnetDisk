3972f09e4f42517fa15ca3f3d093f4143cc77281
fanzhouzhou <3195063800@qq.com>
2022-10-13 11:19:35 +0800

    注册逻辑修改

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -184,11 +184,12 @@ export default {
         .then(res => {
           this.codeLoading = false;
           this.isDisabled = true;
+          this.$message1000("验证码已发送");
           let time = 60;
           let timer = setInterval(() => {
             this.info = `${time--} S`;
             if (this.info <= 0) {
-              this.info = "发送验证码";
+              this.time = "发送验证码";
               this.isDisabled = false;
               clearInterval(timer);
             }
