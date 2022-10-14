ddaf43bf3597727d7777258e2415d1119c03248e
fanzhouzhou <3195063800@qq.com>
2022-10-14 16:49:22 +0800

    修改url

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ const path = require('path')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
-// const url = 'http://localhost:5000'
-const url = 'http://82.157.234.218:5000'
+const url = 'http://localhost:5000'
+// const url = 'http://82.157.234.218:5000'
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 module.exports = {
